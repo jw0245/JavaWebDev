201691d90ff47424d67c16447ecbc736efe4c30a
LJW <61214962+jw0245@users.noreply.github.com>
2022-01-16 20:40:01 +0900

    문제풀이

--- src/Test/First_test.java
+++ src/Test/First_test.java
@@ -1,5 +1,7 @@
 package Test;
 
+import java.util.Scanner;
+
 public class First_test {
 
     /* 1. 연산자와 두수를 변수로 선언한 후 사칙연산이 수행되는 프로그램 구현
@@ -15,6 +17,32 @@ public class First_test {
                 *
      */
 
+    public static void main(String[] args) {
 
+        // 1번 문제
+        int n = 100;
+        int m = 20;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("연산을 입력해 주세요 : ");
+        String con = scanner.next();
+
+        if(con.equals("+")){
+            System.out.println(n+m);
+        }
+        else if(con.equals("-")){
+            System.out.println(n-m);
+        }
+        else  if(con.equals("x")){
+            System.out.println(n*m);
+        }
+        else if(con.equals("/")){
+            System.out.println(n/m);
+        }
+        else{
+            System.out.println("잘못 입력하여 종료합니다.");
+        }
+
+    }
 
 }
---
+++ src/Test/First_test2.java
@@ -0,0 +1,19 @@
+package Test;
+
+import java.util.Scanner;
+
+public class First_test2 {
+    public static void main(String[] args) {
+        // 스위치문을 이용한 문제1
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        int n = 100;
+        int m = 20;
+        int result = 0;
+        switch (input){
+            case 1 : result = n+m;
+                System.out.println();
+        }
+
+    }
+}
