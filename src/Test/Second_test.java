ba06b5cf62c44df3d5d943993a62381dfad37c71
gw0245 <xx6677as@gmail.com>
2022-01-17 18:05:37 +0900

    클래스와 매소드 관련 학습

--- src/Test/Second_test.java
+++ src/Test/Second_test.java
@@ -1,2 +1,46 @@
-package Test;public class Second_test {
+package Test;
+
+public class Second_test {
+    /*쇼핑몰에서 주문이 들어왔씁니다.
+            주문 번호 : 201907210001
+            주문자 아이디 : abc123
+            주문자 날짜 : 2019년 7월 21일
+            주문자 이름: 홍길순
+            주문 상품 번호 : PD0345-12
+            배송 주소 : 서울시 영등포구 여의도동 20번지
+        *
+
+        */
+    String order_num;
+    String order_id;
+    String date;
+    String name;
+    String order_productno;
+    String address;
+    public void showOrderInfo(){
+
+        System.out.println("주문 번호: " + order_num );
+        System.out.println("주문자 아이디: " + order_id );
+        System.out.println("주문자 날짜" + date);
+        System.out.println("이름 : " + name);
+        System.out.println("주문 상품 번호" + order_productno);
+        System.out.println("배송 주소" + address);
+
+    }
+
+    public static void main(String[] args) {
+        Second_test shop = new Second_test();
+
+        shop.order_num = "201907210001";
+        shop.order_id = "abc123";
+        shop.date = "2019년 7월 21일";
+        shop.name = "홀길순";
+        shop.order_productno = "PD0345-12";
+        shop.address = "서울시 영등포구 여의도동 20번지";
+
+        shop.showOrderInfo();
+
+
+
+    }
 }
--- src/classpart/Student.java
+++ src/classpart/Student.java
@@ -1,2 +1,24 @@
-package classpart;public class Student {
+package classpart;
+
+public class Student {
+    int studentID;
+    String studentName;
+    String address;
+
+    public void showStudentInfo(){
+
+        System.out.println(studentName + "," + address );
+    }
+
+    public String getStudentName(){
+
+        return studentName;
+    }
+
+    public static void main(String[] args) {
+        Student studentPark = new Student();
+        studentPark.studentName = "박은종";
+        studentPark.showStudentInfo();
+    }
+
 }
